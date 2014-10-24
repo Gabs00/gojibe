2e9b2abf3625f3fca27857d3947581bea7d0b6c2
Gabs00 <gewen87@gmail.com>
2014-10-24 07:48:13 -0400

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('gojibeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
